b88acd4ef67d9df2b0b7406a897e51e79ed6dc0f
glamridgerider <lolavelorosa@gmail.com>
2025-06-05 16:51:50 +0200

    Update - URLs in Event Data parsing fixed

--- src/js/events-data.js
+++ src/js/events-data.js
@@ -10,13 +10,13 @@ export const calendarEvents = [
             rideType: 'Longer Ride',
             departureTime: '09:00',
             departLocation: 'Regular Meeting Point',
-            departLocationURL: 'https://www.google.com/maps/place/The+Views,+Views+Recreation+Ground+car+park,+Farnborough+GU14+0EE/@51.2966581,-0.7557221,17z',
+            departLocationURL: 'https://www.google.com/maps/place/The+Views+Recreation+Ground+Car+Park/@51.2966581,-0.7557221,17z',
             distance: '38 miles',
             rideTime: '3 hours',
             guidance: `Mixed riding including Tarmac, a fun challenging ride!
 This is not the usual near flat ride!
 Clockwise loop to Guildford, via Ash, Pirbright, Guildford.
-[Lunch stop at Watts Gallery](https://www.google.com/maps/place/Watts+Gallery+-+Artists'+Village/@51.2184989,-0.6267329,17z/data=!3m1!4b1!4m6!3m5!1s0x4875d80082177b49:0x20fb7c18af851e89!8m2!3d51.2184989!4d-0.624158!16s%2Fm%2F03nq_gx)
+[Lunch stop at Watts Gallery](https://www.google.com/maps/place/Watts+Gallery+Artists+Village/@51.2184989,-0.6267329,17z)
 Then onto Farnham and Fleet. 
 For route details and photos, visit https://app.box.com/s/8x8r3uwnpdknokz5fbxnmq03pouj3e9x
 Make sure you have plenty of fluids and snacks!`,
@@ -31,7 +31,7 @@ Make sure you have plenty of fluids and snacks!`,
             rideType: 'Regular Ride',
             departureTime: '09:30',
             departLocation: 'Regular Meeting Point',
-            departLocationURL: 'https://www.google.com/maps/place/The+Views,+Views+Recreation+Ground+car+park,+Farnborough+GU14+0EE/@51.2966581,-0.7557221,17z',
+            departLocationURL: 'https://www.google.com/maps/place/The+Views+Recreation+Ground+Car+Park/@51.2966581,-0.7557221,17z',
             distance: '20 miles',
             rideTime: '90-120 minutes, 8-10mph',
             guidance: `If driving to Meet Point, the car park is a pay car park. Alternatively park for free on Dinorben Avenue (1 minute from start): Free Parking  .
@@ -48,7 +48,7 @@ All of these routes offer the option of a shorter ride, please ask for advice.`,
             rideType: 'Regular Ride',
             departureTime: '09:30',
             departLocation: 'Regular Meeting Point',
-            departLocationURL: 'https://www.google.com/maps/place/The+Views,+Views+Recreation+Ground+car+park,+Farnborough+GU14+0EE/@51.2966581,-0.7557221,17z',
+            departLocationURL: 'https://www.google.com/maps/place/The+Views+Recreation+Ground+Car+Park/@51.2966581,-0.7557221,17z',
             distance: '20 miles',
             rideTime: '90-120 minutes, 8-10mph',
             guidance: `If driving to Meet Point, the car park is a pay car park. Alternatively park for free on Dinorben Avenue (1 minute from start).
@@ -64,7 +64,7 @@ All of these routes offer the option of a shorter ride, please ask for advice.`,
             rideType: 'Regular Ride',
             departureTime: '09:30',
             departLocation: 'Regular Meeting Point',
-            departLocationURL: 'https://www.google.com/maps/place/The+Views,+Views+Recreation+Ground+car+park,+Farnborough+GU14+0EE/@51.2966581,-0.7557221,17z',
+            departLocationURL: 'https://www.google.com/maps/place/The+Views+Recreation+Ground+Car+Park/@51.2966581,-0.7557221,17z',
             distance: '20 miles',
             rideTime: '90-120 minutes, 8-10mph',
             guidance: `If driving to Meet Point, the car park is a pay car park. Alternatively park for free on Dinorben Avenue (1 minute from start).
@@ -80,7 +80,7 @@ All of these routes offer the option of a shorter ride, please ask for advice.`,
             rideType: 'Regular Ride',
             departureTime: '09:30',
             departLocation: 'Regular Meeting Point',
-            departLocationURL: 'https://www.google.com/maps/place/The+Views,+Views+Recreation+Ground+car+park,+Farnborough+GU14+0EE/@51.2966581,-0.7557221,17z',
+            departLocationURL: 'https://www.google.com/maps/place/The+Views+Recreation+Ground+Car+Park/@51.2966581,-0.7557221,17z',
             distance: '20 miles',
             rideTime: '90-120 minutes, 8-10mph',
             guidance: `If driving to Meet Point, the car park is a pay car park. Alternatively park for free on Dinorben Avenue (1 minute from start).
@@ -96,11 +96,11 @@ All of these routes offer the option of a shorter ride, please ask for advice.`,
             rideType: 'Social',
             departureTime: '09:30',
             departLocation: 'Regular Meeting Point',
-            departLocationURL: 'https://www.google.com/maps/place/The+Views,+Views+Recreation+Ground+car+park,+Farnborough+GU14+0EE/@51.2966581,-0.7557221,17z',
+            departLocationURL: 'https://www.google.com/maps/place/The+Views+Recreation+Ground+Car+Park/@51.2966581,-0.7557221,17z',
             distance: '20 miles',
-            rideTime: '90-120 minutesm 8-10mph',
+            rideTime: '90-120 minutes, 8-10mph',
             guidance: `Bring your brolly and a chair, and a plate of food to share.
-            afterwards beers at [The Heron](https://www.google.com/maps/place/14+Old+Cove+Rd,+Fleet+GU51+2RY,+UK/@51.2921671,-0.8280629,17z/data=!3m1!4b1!4m6!3m5!1s0x48742bcf50fee825:0x1b71f1c34f69c94b!8m2!3d51.2921671!4d-0.8280629!16s%2Fg%2F11c3q5h93f?entry=ttu&g_ep=EgoyMDI1MDYwMi4wIKXMDSoASAFQAw%3D%3D)`,
+Afterwards beers at [The Heron](https://www.google.com/maps/place/The+Heron+On+Fleet/@51.2921671,-0.8280629,17z)`,
             ridePhotosURL: 'https://example.com/rides/2025-06-12'
         }
     }
